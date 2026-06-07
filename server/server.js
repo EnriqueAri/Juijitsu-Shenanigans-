d248e45897ddb29c8ab8731436718215727df7f9
EnriqueAri <arihaanruhela@gmail.com>
2026-06-07 15:24:49 -0700

    Refactor server.js to improve broadcast functionality

--- server/server.js
+++ server/server.js
@@ -5,26 +5,37 @@ const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Juijitsu Shenanigans Multiplayer Server Running!");
+    res.end("JJS Multiplayer Server Running");
 });
 
 const wss = new WebSocket.Server({ server });
 
 const players = {};
 
+function broadcast(data) {
+    const msg = JSON.stringify(data);
+
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(msg);
+        }
+    });
+}
+
 wss.on("connection", (ws) => {
 
     const id =
         Math.random().toString(36).substring(2, 9);
 
-  players[id] = {
-    id,
-    username: "Player",
-    x: 0,
-    y: 1,
-    z: 0,
-    hp: 100
-};
+    players[id] = {
+        id,
+        username: "Player",
+        x: 0,
+        y: 1,
+        z: 0,
+        hp: 100
+    };
+
     ws.send(JSON.stringify({
         type: "init",
         id
@@ -46,29 +57,17 @@ wss.on("connection", (ws) => {
                 players[id].y = data.y;
                 players[id].z = data.z;
             }
-            if (data.type === "attack") {
 
-    const packet = JSON.stringify({
-        type: "attack",
-        attack: data.attack,
-        x: data.x,
-        y: data.y,
-        z: data.z,
-        yaw: data.yaw,
-        owner: id
-    });
+            if (
+                data.type === "attack" ||
+                data.type === "domain" ||
+                data.type === "damage"
+            ) {
 
-    wss.clients.forEach(client => {
+                data.owner = id;
 
-        if (
-            client.readyState ===
-            WebSocket.OPEN
-        ) {
-            client.send(packet);
-        }
-
-    });
-}
+                broadcast(data);
+            }
 
         } catch (err) {
             console.error(err);
@@ -77,33 +76,26 @@ wss.on("connection", (ws) => {
     });
 
     ws.on("close", () => {
+
         delete players[id];
+
     });
 
 });
 
 setInterval(() => {
 
-    const snapshot = JSON.stringify({
+    broadcast({
         type: "players",
         players
-    });
-
-    wss.clients.forEach(client => {
-
-        if (
-            client.readyState ===
-            WebSocket.OPEN
-        ) {
-            client.send(snapshot);
-        }
-
     });
 
 }, 50);
 
 server.listen(PORT, "0.0.0.0", () => {
+
     console.log(
         `Server running on port ${PORT}`
     );
+
 });
