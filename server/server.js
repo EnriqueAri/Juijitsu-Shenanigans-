53ec9cd43126a176a61707859efa349de2341958
EnriqueAri <arihaanruhela@gmail.com>
2026-06-09 11:17:47 -0700

    Refactor server.js for improved readability

--- server/server.js
+++ server/server.js
@@ -4,108 +4,115 @@ const { Server } = require("socket.io");
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-res.writeHead(200, { "Content-Type": "text/plain" });
-res.end("JJS Socket.IO Server Running");
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("JJS Socket.IO Server Running");
 });
 
 const io = new Server(server, {
-cors: {
-origin: "*",
-methods: ["GET", "POST"]
-}
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
 });
 
 const players = {};
 
 io.on("connection", (socket) => {
 
-```
-console.log("Player connected:", socket.id);
+    console.log("Player connected:", socket.id);
 
-socket.on("join", (data) => {
+    socket.on("join", (data) => {
 
-    players[socket.id] = {
-        id: socket.id,
-        name: data.name || "Player",
-        char: data.char || "gojo",
-        pos: data.pos || { x: 0, y: 0, z: 0 },
-        hp: data.hp || 100,
-        maxHp: data.maxHp || 100,
-        yaw: 0
-    };
+        players[socket.id] = {
+            id: socket.id,
+            name: data.name || "Player",
+            char: data.char || "gojo",
+            pos: data.pos || { x: 0, y: 0, z: 0 },
+            hp: data.hp || 100,
+            maxHp: data.maxHp || 100,
+            yaw: 0
+        };
 
-    socket.emit("currentPlayers", players);
+        socket.emit("currentPlayers", players);
 
-    socket.broadcast.emit("playerJoined", {
-        id: socket.id,
-        ...players[socket.id]
-    });
-});
+        socket.broadcast.emit("playerJoined", {
+            id: socket.id,
+            ...players[socket.id]
+        });
 
-socket.on("move", (data) => {
-
-    if (!players[socket.id]) return;
-
-    players[socket.id].pos = data.pos || players[socket.id].pos;
-    players[socket.id].yaw = data.yaw || 0;
-    players[socket.id].hp = data.hp || players[socket.id].hp;
-
-    socket.broadcast.emit("playerMoved", {
-        id: socket.id,
-        pos: players[socket.id].pos,
-        yaw: players[socket.id].yaw,
-        hp: players[socket.id].hp
+        console.log("Joined:", players[socket.id].name);
     });
 
-    if (data.respawned) {
-        socket.broadcast.emit("playerRespawned", {
+    socket.on("move", (data) => {
+
+        if (!players[socket.id]) return;
+
+        if (data.pos)
+            players[socket.id].pos = data.pos;
+
+        if (typeof data.yaw === "number")
+            players[socket.id].yaw = data.yaw;
+
+        if (typeof data.hp === "number")
+            players[socket.id].hp = data.hp;
+
+        socket.broadcast.emit("playerMoved", {
             id: socket.id,
             pos: players[socket.id].pos,
+            yaw: players[socket.id].yaw,
             hp: players[socket.id].hp
         });
-    }
 
-    if (data.isUlt) {
-        io.emit("ultUsed", {
+        if (data.respawned) {
+            socket.broadcast.emit("playerRespawned", {
+                id: socket.id,
+                pos: players[socket.id].pos,
+                hp: players[socket.id].hp
+            });
+        }
+
+        if (data.isUlt) {
+            io.emit("ultUsed", {
+                id: socket.id
+            });
+        }
+    });
+
+    socket.on("hitPlayer", (data) => {
+
+        if (!data || !data.targetId) return;
+
+        io.to(data.targetId).emit("playerHit", {
+            targetId: data.targetId,
+            dmg: data.dmg || 0,
+            moveName: data.moveName || "Attack",
+            color: data.color || "#ffffff"
+        });
+    });
+
+    socket.on("playerDied", () => {
+
+        socket.broadcast.emit("playerDied", {
             id: socket.id
         });
-    }
-});
-
-socket.on("hitPlayer", (data) => {
-
-    io.to(data.targetId).emit("playerHit", {
-        targetId: data.targetId,
-        dmg: data.dmg,
-        moveName: data.moveName,
-        color: data.color
     });
-});
 
-socket.on("playerDied", () => {
+    socket.on("ping_jjs", (timestamp) => {
 
-    socket.broadcast.emit("playerDied", {
-        id: socket.id
+        socket.emit("pong_jjs", timestamp);
     });
-});
 
-socket.on("ping_jjs", (timestamp) => {
+    socket.on("disconnect", () => {
 
-    socket.emit("pong_jjs", timestamp);
-});
+        console.log("Player disconnected:", socket.id);
 
-socket.on("disconnect", () => {
+        delete players[socket.id];
 
-    console.log("Player disconnected:", socket.id);
-
-    delete players[socket.id];
-
-    io.emit("playerLeft", socket.id);
-});
-```
+        io.emit("playerLeft", socket.id);
+    });
 
 });
 
 server.listen(PORT, "0.0.0.0", () => {
-console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
